8a853fe193fed4ec9ac6eee7c270d284c4e1bf80
DaniruKun <thedanpetrov@gmail.com>
2023-02-18 01:56:32 +0100

    Combine whisperx scripts into one

--- whisperx-transcribe.py
+++ whisperx-transcribe.py
@@ -1,17 +1,67 @@
+"""
+A memory-optimised version of the stock WhisperX script, which unloads and GCs garbage in memory and in CUDA cache, before attempting alignment.
+"""
 import whisperx
-import pickle
 import sys
+import gc
+import torch
 
-device = "cuda" 
+from typing import Iterator, TextIO
+
 audio_file = sys.argv[1]
+device = 'cuda'
 
-# transcribe with original whisper
-model = whisperx.load_model("medium.en", device)
-result = model.transcribe(audio_file)
 
-temp_file_name = audio_file + '.results.pkl'
+def format_timestamp(seconds: float, always_include_hours: bool = False, decimal_marker: str = '.'):
+    assert seconds >= 0, "non-negative timestamp expected"
+    milliseconds = round(seconds * 1000.0)
 
-with open(temp_file_name, 'wb') as f:
-    pickle.dump(result, f)
+    hours = milliseconds // 3_600_000
+    milliseconds -= hours * 3_600_000
 
-print('Dumped transcription results to temp pickle file: ', temp_file_name)
+    minutes = milliseconds // 60_000
+    milliseconds -= minutes * 60_000
+
+    seconds = milliseconds // 1_000
+    milliseconds -= seconds * 1_000
+
+    hours_marker = f"{hours:02d}:" if always_include_hours or hours > 0 else ""
+    return f"{hours_marker}{minutes:02d}:{seconds:02d}{decimal_marker}{milliseconds:03d}"
+
+
+def write_vtt(transcript: Iterator[dict], file: TextIO):
+    print("WEBVTT\n", file=file)
+    for segment in transcript:
+        print(
+            f"{format_timestamp(segment['start'])} --> {format_timestamp(segment['end'])}\n"
+            f"{segment['text'].strip().replace('-->', '->')}\n",
+            file=file,
+            flush=True,
+        )
+
+
+whisper_model = whisperx.load_model("medium.en", device)
+result = whisper_model.transcribe(audio_file)
+
+# Garbage collect everything before loading alignment model
+del whisper_model
+gc.collect()
+torch.cuda.empty_cache()
+
+input('Press enter to proceed with alignment...')
+
+# load alignment model and metadata
+model_a, metadata = whisperx.load_align_model(
+    language_code=result["language"], device=device)
+
+# align whisper output
+result_aligned = whisperx.align(
+    result["segments"], model_a, metadata, audio_file, device)
+
+for segment in result_aligned["segments"]:
+    print(format_timestamp(segment['start']), segment['text'])
+
+with open(audio_file + ".vtt", 'w', encoding='utf-8') as vtt:
+    write_vtt(result_aligned['segments'], file=vtt)
+
+print("wrote VTT sub file to ", audio_file + '.vtt')
